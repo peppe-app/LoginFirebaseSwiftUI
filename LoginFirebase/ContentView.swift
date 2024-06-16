d9818fa37613e795591800f0f89e4a9ab38dfff5
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2024-06-16 14:58:46 +1000

    Initial Commit

---
+++ LoginFirebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginFirebase
+//
+//  Created by Giuseppe Sapienza on 16/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginFirebase/LoginFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFirebaseApp.swift
+//  LoginFirebase
+//
+//  Created by Giuseppe Sapienza on 16/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
